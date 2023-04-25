d0c8b24847620573938f090ed2d021939bdb8e78
Pedro Moutinho <phpedromoutinho@gmail.com>
2023-04-25 16:16:26 -0300

    Parte 1 finalizada - Testes 1 a 6 funcionais

--- dccthread.c
+++ dccthread.c
@@ -14,6 +14,7 @@ typedef struct dccthread{
 
 dccthread_t *gerente;
 dccthread_t *principal;
+dccthread_t *thread_atual;
 
 struct dlist *threads_prontas;
 struct dlist *threads_aguardando;
@@ -24,8 +25,11 @@ const char * dccthread_name(dccthread_t *tid){
 	return tid->name;
 }
 
-void set_initial_context(dccthread_t *thread) {
-    thread->context.uc_link = NULL;
+void set_initial_context(dccthread_t *thread, dccthread_t *link_thread) {
+    if (link_thread != NULL) {
+        thread->context.uc_link = &link_thread->context;
+    }
+    
     thread->context.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
     thread->context.uc_stack.ss_size = THREAD_STACK_SIZE;
     thread->context.uc_stack.ss_flags = 0;
@@ -37,12 +41,22 @@ dccthread_t * dccthread_create(const char *name, void (*func)(int), int param) {
     getcontext(&nova_thread->context);
     strcpy(nova_thread->name, name);
     
-    set_initial_context(nova_thread);
+    set_initial_context(nova_thread, gerente);
 
     // Adiciona a thread a lista de threads prontas
     dlist_push_right(threads_prontas, nova_thread);
     makecontext(&nova_thread->context, (void *) func, 1, param);
     return nova_thread;
+}
+
+void gerenciador() {
+    while(!dlist_empty(threads_prontas)) {
+        dccthread_t *thread = (dccthread_t *) malloc (sizeof(dccthread_t));
+        thread = threads_prontas->head->data;
+        swapcontext(&gerente->context, &thread->context);
+        dlist_pop_left(threads_prontas);
+    }
+    exit(EXIT_SUCCESS);
 }
 
 void dccthread_init(void (*func)(int), int param) {
@@ -54,12 +68,23 @@ void dccthread_init(void (*func)(int), int param) {
     getcontext(&gerente->context);
     strcpy(gerente->name, "gerente");
 
-    set_initial_context(gerente);
+    set_initial_context(gerente, NULL);
+    makecontext(&gerente->context, (void *) gerenciador, 0);
 
     principal = dccthread_create("main", func, param);
-    while(!dlist_empty(threads_prontas)) {
-        dccthread_t *thread = dlist_pop_left(threads_prontas);
-        swapcontext(&gerente->context, &thread->context);
-    }
+
+    setcontext(&gerente->context);
+   
     exit(EXIT_SUCCESS);
+}
+
+dccthread_t * dccthread_self(void){
+	return threads_prontas->head->data;
+}
+
+void dccthread_yield(void){
+	dccthread_t *current_context = dccthread_self();
+
+	dlist_push_right(threads_prontas, current_context);
+	swapcontext(&current_context->context, &gerente->context);
 }
