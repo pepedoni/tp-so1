c702c5d641546888fd1c4acb7dbf445b0d736ed5
Pedro Moutinho <phpedromoutinho@gmail.com>
2023-04-25 15:47:16 -0300

    Pass in test 1 and 2

--- dccthread.c
+++ dccthread.c
@@ -18,23 +18,30 @@ dccthread_t *principal;
 struct dlist *threads_prontas;
 struct dlist *threads_aguardando;
 
-void t1(int param) {
-    printf("t1: %d", param);
-    exit(EXIT_SUCCESS);
-}
+int total_threads = 0;
 
 const char * dccthread_name(dccthread_t *tid){
 	return tid->name;
+}
+
+void set_initial_context(dccthread_t *thread) {
+    thread->context.uc_link = NULL;
+    thread->context.uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
+    thread->context.uc_stack.ss_size = THREAD_STACK_SIZE;
+    thread->context.uc_stack.ss_flags = 0;
 }
 
 dccthread_t * dccthread_create(const char *name, void (*func)(int), int param) {
     dccthread_t *nova_thread;
 	nova_thread = (dccthread_t*) malloc (sizeof(dccthread_t));
     getcontext(&nova_thread->context);
+    strcpy(nova_thread->name, name);
+    
+    set_initial_context(nova_thread);
 
     // Adiciona a thread a lista de threads prontas
     dlist_push_right(threads_prontas, nova_thread);
-    makecontext(&nova_thread->context, (void (*)(void)) func, 1, param);
+    makecontext(&nova_thread->context, (void *) func, 1, param);
     return nova_thread;
 }
 
@@ -47,13 +54,12 @@ void dccthread_init(void (*func)(int), int param) {
     getcontext(&gerente->context);
     strcpy(gerente->name, "gerente");
 
+    set_initial_context(gerente);
+
     principal = dccthread_create("main", func, param);
     while(!dlist_empty(threads_prontas)) {
         dccthread_t *thread = dlist_pop_left(threads_prontas);
         swapcontext(&gerente->context, &thread->context);
     }
-    exit(0);
-}
-void main() {
-    dccthread_init(t1, 1);
+    exit(EXIT_SUCCESS);
 }
